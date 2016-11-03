47c62cbfaedd8efade4019a141ac3832e068aeec
Tiago Cardoso <tiago@restorm.com>
2016-11-03 23:08:24 +0100

    updated the example

--- examples/echo_server.rb
+++ examples/echo_server.rb
@@ -2,13 +2,17 @@ require 'jaguar'
 
 Response = Struct.new(:status, :headers, :body)
 
-APP = ->(req) do
+APP = ->(req, res) do
   body = "echo!!!!!!"
   headers = {"content-type" => "text/plain", "content-length" => body.bytesize.to_s}
-  Response.new(200, headers, [body])
+  res.status = 200
+  res.headers["content-type"] = "text/plain"
+  res.headers["content-length"] = body.bytesize.to_s 
+  res.body = [body]
 end
 
-server = Jaguar::Server.new("localhost", 9292, action: APP)
+tcp_server = TCPServer.new("localhost", 9292)
+server = Jaguar::Server.new(tcp_server, action: APP)
 
 puts "server is on..."
 server.run
