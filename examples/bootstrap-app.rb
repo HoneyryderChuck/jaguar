886118b17a25848b35625cd19331dbe6646ecc8b
Tiago Cardoso <tiago@restorm.com>
2016-11-23 01:38:13 +0100

    adding push example (push is being sent, but request is still coming from client...

--- examples/bootstrap-app.rb
+++ examples/bootstrap-app.rb
@@ -3,11 +3,24 @@ require "pathname"
 
 ROOT = Pathname.new(File.dirname(__FILE__)).join("bootstrap-app")
 
+Promise = Struct.new(:status, :headers, :body)
+
 APP = ->(req, res) do
   file = ROOT.join(req.url[1..-1]) # trailing "/"
   if file.file? or not ROOT.children.include?(file)
+    puts "serving #{req.url}..."
+
     ext = file.extname[1..-1] # trailing "."
     body = file.read
+    # promise
+    if ext.end_with?("html")
+      bootbody = ROOT.join("css", "bootstrap.min.css").read
+      bootstrap_css = Promise.new(200, {"content-length" => bootbody.bytesize.to_s,
+                                        "content-type" => "text/css"}, [bootbody])
+      puts "pushing /css/bootstrap.min.css..."
+      res.push "/css/bootstrap.min.css", bootstrap_css
+    end
+
     res.headers["content-type"] = "text/#{ext}"
     res.headers["content-length"] = body.bytesize.to_s
     res.body = [body]
