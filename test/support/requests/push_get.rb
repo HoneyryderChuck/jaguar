6130c0c5c3849eeeffb5fa940f5f7301522c370a
Tiago Cardoso <tiago@restorm.com>
2016-11-25 12:56:02 +0100

    the push test is now testing with the Link header, which will be the default to handle server push when we are the pusher

--- test/support/requests/push_get.rb
+++ test/support/requests/push_get.rb
@@ -4,18 +4,19 @@ module Requests
     Promise = Struct.new(:status, :headers, :body)
 
     def test_push_get
+      assets_dir = Dir.mktmpdir("test_assets")
+      File.open(File.join(assets_dir, "main.css"), "w+") { |f| f.write(".rule {}") } 
+
       server.run do |req, rep|
         if req.url == "/"
           rep.body = %w(Right)
           rep.headers["content-type"] = "text/plain"
           rep.headers["content-length"] = 5
 
-          promise = Promise.new(200, 
-                                {"content-length" => "4", 
-                                 "content-type"   => "text-plain"}, 
-                                 %w(Time))
+          rep.headers.add_field("Link", "</main.css>; rel=preload; as=style")
 
-          rep.push "/resource", promise
+
+          rep.enable_push!([assets_dir])
         else
           rep.status = 400
           rep.headers["content-type"] = "text/plain"
@@ -34,13 +35,11 @@ module Requests
       promise = client.promise
 
       assert promise.status == 200, "promise status code is unexpected"
-      assert promise.headers["content-length"].include?("4"), "promise content length is unexpected"
-      assert promise.body == "Time", "promise body is unexpected"
-      
-    ensure
-      client.close if client
+      assert promise.headers["content-type"].include?("text/css"), "promise content type is unexpected"
+      assert promise.body == ".rule {}", "promise body is unexpected"
+    ensure 
+      FileUtils.rm_rf(assets_dir) if assets_dir
     end
-
 
   end
 end
