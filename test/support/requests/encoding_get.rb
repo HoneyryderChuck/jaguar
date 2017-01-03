6eb0fbb54946b953ea648e31b4c4e9f5cee7bdc4
Tiago Cardoso <tiago@restorm.com>
2017-01-03 14:11:40 +0100

    adding deflate encoding spec

--- test/support/requests/encoding_get.rb
+++ test/support/requests/encoding_get.rb
@@ -22,6 +22,26 @@ module Requests
       assert transfer_encoding(response).include?("gzip"), "response encoding is unexpected"
     end
 
+    def test_encoding_deflate
+      server.run do |req, rep|
+        if req.url == "/"
+          rep.body = %w(Right)
+          rep.headers["content-length"] = rep.body.map(&:bytesize).reduce(:+)
+        else
+          rep.status = 400
+          rep.body = %w(Wrong)
+          rep.headers["content-length"] = rep.body.map(&:bytesize).reduce(:+)
+        end
+      end 
+
+      response = client.request(:get,"#{server_uri}/", 
+                                     headers: {"accept" => "*/*",
+                                               "accept-encoding" => "deflate"})
+
+      assert response.status == 200, "response status code is unexpected"
+      assert transfer_encoding(response) != nil, "response encoding is unexpected"
+      assert transfer_encoding(response).include?("deflate"), "response encoding is unexpected"
+    end
 
 
     def transfer_encoding(response)
