4707be6c9ea1a36a13a34fa8283a99fd157261cd
Tiago Cardoso <tiago@restorm.com>
2017-01-24 16:40:39 +0100

    when debugging an http1 request and the body is humongous, just show the diget

--- lib/jaguar/http1/request.rb
+++ lib/jaguar/http1/request.rb
@@ -24,11 +24,18 @@ module Jaguar::HTTP1
 
     def to_s(*args)
       if args.first.respond_to?(:[]) && args.first[:debug]
-        "HTTP1 #{url} #{verb}\n" +
-        headers.to_hash.map { |k,v| "#{k}: #{v}" }.join("\n") +
-        "\n" +
-        body.to_a.join +
-        "\n"
+        buffer = String.new
+        buffer << "HTTP1 #{url} #{verb}\n"
+        buffer << headers.to_hash.map { |k,v| "#{k}: #{v}" }.join("\n")
+        buffer << "\n"
+        b = body.to_a.join
+        b.force_encoding("UTF-8")
+        buffer << if b.bytesize > 1024
+          Digest::SHA256.new.digest(b)
+        else
+          b
+        end
+        buffer << "\n"
       else
         super
       end
